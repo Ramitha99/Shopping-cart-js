3792ede0246ad248e795c0091368dba30cca797d
Ramitha Manathunga <ramithadm@gmail.com>
2022-07-14 10:04:34 +0530

    add functionalities
    added function to remove all item from the cart.
    added function to calculate total amount in the cart

--- src/cart.js
+++ src/cart.js
@@ -16,16 +16,17 @@ let generateCartItems = () => {
       .map((x) => {
         let { id, item } = x;
         let search = shopItemData.find((y) => y.id === id) || [];
+        let { img, name, price } = search;
         return ` <div class="cart-item">
-        <img width= "100" src=${search.img} alt="" />
-        <div class="details">
+        <img width= "100" src=${img} alt="" />
+        <div class="detail">
 
           <div class="title-price-x">
             <h4 class="title-price">
-              <p>${search.name}</p>
-              <p class="cart-item-price">$ ${search.price}</p>
+              <p>${name}</p>
+              <p class="cart-item-price">$ ${price}</p>
             </h4>
-            <i class="bi bi-x-lg"></i>
+            <i onclick="removeItem(${id})"class="bi bi-x-lg"></i>
           </div>
 
           <div class="buttons">
@@ -93,6 +94,41 @@ let update = (id) => {
   //   console.log(search.item);
   document.getElementById(id).innerHTML = search.item;
   calculation();
+  totalAmount();
 };
 
-let removeItem = (id) => {}
+let removeItem = (id) => {
+  let selectedItem = id;
+  // console.log(selectedItem.id);
+  basket = basket.filter((x) => x.id !== selectedItem.id);
+  generateCartItems();
+  localStorage.setItem("data", JSON.stringify(basket));
+  totalAmount();
+  calculation();
+};
+
+let clearCart = () => {
+  basket = [];
+  generateCartItems();
+  localStorage.setItem("data", JSON.stringify(basket));
+  calculation();
+};
+
+let totalAmount = () => {
+  if (basket.length !== 0) {
+    let amount = basket
+      .map((x) => {
+        let { item, id } = x;
+        let search = shopItemData.find((y) => y.id === id) || [];
+        return item * search.price;
+      })
+      .reduce((x, y) => x + y, 0);
+    label.innerHTML = `
+      <h2>Total Bill : $ ${amount}</h2>
+      <button class="checkout">Checkout</button>
+      <button onclick="clearCart()" class="removeAll">Clear</button>
+      `;
+  } else return;
+};
+
+totalAmount();
